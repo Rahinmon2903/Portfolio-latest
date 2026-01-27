9e631ca41c7937bb542ca935e71132822e93f4af
Rahinmon2903 <rahin2903@gmail.com>
2026-01-27 11:38:27 +0530

    path added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import ProjectDetail from "./pages/ProjectDetail";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
-const App = () => {
+export default function App() {
   return (
-    <div>
-      <h1>hello</h1>
-      
+    <div className="min-h-screen bg-black text-white">
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/:slug" element={<ProjectDetail />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
-};
-
-export default App;
+}
